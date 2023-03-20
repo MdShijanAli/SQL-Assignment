0fd29efdb00aa1c51c13a418ff337427fa8ac515
FreelancerShijan <freelancershijan@gmail.com>
2023-03-21 00:38:23 +0600

    finished

--- sql.php
+++ sql.php
@@ -15,3 +15,50 @@ SELECT name, salary FROM employees WHERE salary > 50000;
 
 
 <!-- c. Write a query to calculate the average salary of all employees. -->
+
+SELECT AVG(salary) FROM employees;
+
+
+<!-- d. Write a query to count the number of employees who work in the "Marketing" department. -->
+
+SELECT COUNT(Marketing) FROM employees;
+
+
+<!-- e. Write a query to update the salary column of the employee with an id of 1001 to 60000. -->
+
+UPDATE employees
+SET salary=50000
+WHERE id BETWEEN 1001 to 60000;
+
+
+
+
+<!-- f. Write a query to delete all employees whose salary is less than 30000. -->
+
+DELETE FROM employees WHERE salary < 30000; <!-- Use the departments table to answer the following questions: -->
+
+
+    <!-- a. Write a query to select all columns and rows from the departments table. -->
+
+    SELECT * FROM departments;
+
+
+
+    <!-- b. Write a query to select only the name and manager columns of the "Finance" department. -->
+
+    SELECT name, manager FROM depertments WHERE depertment = "Finance";
+
+
+
+    <!-- c. Write a query to calculate the total number of employees in each department. -->
+
+    SELECT employees, COUNT(*)
+    FROM depertments
+    GROUP BY employees;
+
+
+
+    <!-- d. Write a query to insert a new department called "Research" with a manager named "John Doe". -->
+
+    INSERT INTO departments (department)
+    VALUES ('Research');
