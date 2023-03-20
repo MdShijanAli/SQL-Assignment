196f8ca29debe0a3bd7a0cf93c3264860fa1f6e4
FreelancerShijan <freelancershijan@gmail.com>
2023-03-20 23:29:29 +0600

    assignemnt

---
+++ sql.php
@@ -0,0 +1,17 @@
+<!-- Instructions:
+Use the employees table to answer the following questions: -->
+
+
+<!-- a. Write a query to select all columns and rows from the employees table. -->
+
+SELECT * FROM employees;
+
+
+
+<!-- b. Write a query to select only the name and salary columns of all employees with a salary greater than 50000. -->
+
+SELECT name, salary FROM employees WHERE salary > 50000;
+
+
+
+<!-- c. Write a query to calculate the average salary of all employees. -->
